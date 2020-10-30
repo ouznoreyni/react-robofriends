eebcf2afcb70728e94d97b0ae9bba48763dcf49c
Ousmane DIOP <ousmanediopp268@gmail.com>
2020-10-30 20:30:20 +0000

    update: change hard coded user into json placer api using axios

--- src/App.js
+++ src/App.js
@@ -1,38 +1,45 @@
-import React, {Component} from 'react';
-
-import './App.css';
-import {robots} from './components/robot';
-import Error from './components/layout/errorBoundary'
-import CardList from './components/cards/CardList';
-import SearchBox from './components/layout/searchBox';
-
+import axios from "axios";
+import React, { Component } from "react";
+import "./App.css";
+import CardList from "./components/cards/CardList";
+import Error from "./components/layout/errorBoundary";
+import SearchBox from "./components/layout/searchBox";
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      robots:robots,
-      searchField:''
-    }
-    this.onSearchChange = this.onSearchChange.bind(this)
+      robots: [],
+      searchField: "",
+    };
+    this.onSearchChange = this.onSearchChange.bind(this);
   }
-  onSearchChange(e){
-    this.setState({searchField:e.target.value}); 
+  onSearchChange(e) {
+    this.setState({ searchField: e.target.value });
   }
 
-  render(){
-     const robotFiltered = this.state.robots.filter((robot, id) =>{
-      return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
-    })
+  async componentDidMount() {
+    const { data: robots } = await axios.get(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+    this.setState({ robots });
+  }
+
+  render() {
+    const robotFiltered = this.state.robots.filter((robot, id) => {
+      return robot.name
+        .toLowerCase()
+        .includes(this.state.searchField.toLowerCase());
+    });
     return (
-      <div className='tc'>
-        <h1 className='f2'>RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />    
+      <div className="tc">
+        <h1 className="f2">RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
         <Error>
           <CardList robots={robotFiltered} />
         </Error>
       </div>
-  );
+    );
   }
 }
 
